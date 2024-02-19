b9cf655f2694f28bb4c0116a6f413f9264bd79a9
Amanda Heloisa <amandah.heloisa@gmail.com>
2024-02-18 21:03:34 -0300

    alteração de codigo

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
+const readline = require('readline');
+
+const leitor = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
 const palavraDoDia = 'amigo'
-const palavraDigitada = 'raios'
 
-const resultado = []
-
-for(const letraPalavraDoDia of palavraDoDia) {
-   for(const letraPalavraDigitada of palavraDigitada){
-        if(letraPalavraDigitada === letraPalavraDoDia){
+function verificarPalavra(palavraDigitada) {
+    const resultado = []
+    for(const i in palavraDigitada) {
+        const letraPalavraDigitada = palavraDigitada[i]
+        if(palavraDigitada[i] === palavraDoDia[i]){
             resultado.push('\x1b[36m' + letraPalavraDigitada + '\x1b[0m')
         } else if(palavraDoDia.includes(letraPalavraDigitada)) {
             resultado.push('\x1b[33m' + letraPalavraDigitada + '\x1b[0m')
         } else {
             resultado.push('\x1b[31m' + letraPalavraDigitada + '\x1b[0m')
         }
-    }
+     }
+     console.log(...resultado.slice(0, 5))
 }
 
-console.log(...resultado.slice(0, 5))
+leitor.question('Digite a palavra: ', (palavraDigitada) => {
+    verificarPalavra(palavraDigitada)
+    leitor.close()
+})
